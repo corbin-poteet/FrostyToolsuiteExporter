73cd720793ded1106c1e33a5fba407e23ac0f436
Corbin Poteet <corbin.poteet@gmail.com>
2022-12-30 02:29:33 -0600

    Implement static mesh exporting

--- FrostyPlugin/Windows/BatchExportSelectedWindow.xaml.cs
+++ FrostyPlugin/Windows/BatchExportSelectedWindow.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using Frosty.Controls;
 using Frosty.Core.Controls;
+using FrostySdk.Managers.Entries;
 using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace Frosty.Core.Windows
@@ -15,6 +17,13 @@ namespace Frosty.Core.Windows
     {
         private AssetPath selectedPath = null;
         private IEnumerable itemsSource = null;
+        public bool includeSubDirectories { get; set; } = false;
+
+        public List<string> types = new List<string>()
+        { 
+            "RigidMeshAsset", 
+            "CompositeMeshAsset" 
+        };
 
         public BatchExportSelectedWindow(AssetPath selectedPath, IEnumerable itemsSource)
         {
@@ -27,14 +36,74 @@ namespace Frosty.Core.Windows
         private void doneButton_Click(object sender, RoutedEventArgs e)
         {
             DialogResult = true;
-
             FrostyFolderBrowserDialog fbd = new FrostyFolderBrowserDialog("Batch Export Location");
             if (fbd.ShowDialog())
             {
                 string path = fbd.SelectedPath;
-                Console.WriteLine(path);
-            }
+                string fullPath = selectedPath.FullPath.Trim('/');
 
+                var stopWatch = new Stopwatch();
+                stopWatch.Start();
+
+                List<EbxAssetEntry> entries = new List<EbxAssetEntry>();
+                if (includeSubDirectories)
+                {
+                    foreach (AssetEntry entry in itemsSource)
+                    {
+                        if (entry.Path.Contains(fullPath.ToLower()))
+                        {
+                            if (types.Contains(entry.Type))
+                            {
+                                entries.Add((EbxAssetEntry)entry);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (AssetEntry entry in itemsSource)
+                    {
+                        if (entry.Path.Equals(fullPath, StringComparison.OrdinalIgnoreCase))
+                        {
+                            if (types.Contains(entry.Type))
+                            {
+                                entries.Add((EbxAssetEntry)entry);
+                            }
+                        }
+                    }
+                }
+
+                int initialCount = entries.Count;
+
+                int i = 0;
+                while (entries.Count > 0 && i < entries.Count)
+                {
+                    // use the first instance of a given AssetDefinition to export all instances of that AssetDefinition in the array
+                    AssetDefinition assetDefinition = App.PluginManager.GetAssetDefinition(entries[i].Type) ?? new AssetDefinition();
+                    List<EbxAssetEntry> leftOverEntries = assetDefinition.BatchExport(entries, path, stopWatch);
+                    
+                    // If we somehow failed to export any entries, add 1 to i to avoid an infinite loop
+                    if (leftOverEntries.Count == entries.Count)
+                    {
+                        i++;
+                    }
+
+                    entries = leftOverEntries;
+
+                }
+
+                stopWatch.Stop();
+
+                int finalCount = entries.Count;
+                int totalExported = initialCount - finalCount;
+
+                var ts = stopWatch.Elapsed;
+                string elapsedTime = $"{ts.Seconds}.{ts.Milliseconds}";
+
+                App.Logger.Log("Successfully exported {0} assets in {1} seconds.", totalExported, elapsedTime);
+
+
+            }
             Close();
         }
 
